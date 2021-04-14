ba72a16cf5a2748a3c43880194e483f855d531a0
strikers1999 <mk.jung@xsight.in>
2021-04-14 11:52:11 +0900

    new blockchain add

--- dist/index.js
+++ dist/index.js
@@ -16,4 +16,13 @@ let blockchain = [genesisBLock];
 const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimestamp();
+    const newHash = Block.cacluateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock('Hello'), createNewBlock('Bye Bye'));
 //# sourceMappingURL=index.js.map
